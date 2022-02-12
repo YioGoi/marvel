c455e0ae19ae59f344a2625721351faae5663761
YIGIT DOGAN <dogan.yigit@consultant.turkcell.com.tr>
2022-02-12 21:58:12 +0300

    query fixed

--- src/service/getCharactersService.ts
+++ src/service/getCharactersService.ts
@@ -15,7 +15,7 @@ function serialize(obj: any) {
         if (obj.hasOwnProperty(p)) {
             str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
         }
-    return str.join("&");
+    return '?' + str.join("&");
 }
 
 function getFinalFilterParam(qp: charactersQueryParamTypes) {
